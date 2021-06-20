9f16673997d11cf9ae651b9e54093bedac8a396e
joanjciria <78328085+joanjciria@users.noreply.github.com>
2021-06-20 10:17:20 +0200

    Create persona2.pm

---
+++ Cap 12.Perl orientat a objectes/persona2.pm
@@ -0,0 +1,11 @@
+package Persona; 
+# Classe per emmagatzemar dades sobre una persona
+#persona2.pm 
+use warnings; 
+use strict; 
+sub new { 
+    my $self = {}; 
+    bless ($self, "Persona"); 
+    return $self; 
+} 
+1;
