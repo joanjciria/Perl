164fef717875293c6a6d507a6d7751fe15e6bdd6
joanjciria <78328085+joanjciria@users.noreply.github.com>
2021-06-26 00:32:05 +0200

    Create label3.pl

---
+++ Cap 15.Tk per a Perl/label3.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+#label3.pl
+use warnings;
+use utf8;
+use Tkx;
+ 
+Tkx::wm_title(".", "Etiquetes");
+Tkx::ttk__style_configure("Estil1.TLabel", -background => "red",
+                                            -relief => "sunken");
+ 
+Tkx::ttk__frame(".marc1",  -padding => "5 7 10 12",);
+Tkx::grid( ".marc1", -column => 0, -row => 0, -sticky => "nwes");
+Tkx::image_create_photo( "imgobj", -file => "perlicon.gif");
+Tkx::grid( Tkx::ttk__label(".marc1.etiq1",
+                           -text => "Logo de Perl",
+                           -style => "Estil1.TLabel", -image => "imgobj",
+                           -compound => "top"));
+ 
+Tkx::MainLoop();
